97293b2e486bcfeb27d28a5ef473e0c94dcbf560
User <bruno.serafini@pm.me>
2025-10-04 23:00:42 -0300

    feat: add Task model

---
+++ src/models/task.model.js
@@ -0,0 +1,37 @@
+import { DataTypes, Model } from 'sequelize';
+import { TaskStatus } from '../constants/index.js';
+import Database from '../config/database';
+
+class TaskModel extends Model {}
+
+TaskModel.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING(11),
+      validate: {
+        isIn: [[...TaskStatus]],
+      },
+      defaultValue: 'pending',
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: Database.getInstance(),
+    modelName: 'Task',
+  },
+);
+
+export { TaskModel };
